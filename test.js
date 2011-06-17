8eee6dbbb619f6d28f0f148ec947233797e30586
josephwegner <jwegner@printarts.com>
2011-06-17 12:22:04 +0000

    Initial Commit ~ Code pulled from fileDrop (https://github.com/josephwegner/fileDrop)

---
+++ test.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var session = require('./node-session.js');
+
+var host = "0.0.0.0";
+var port = process.env.C9_PORT
+
+http.createServer(function (req, res) {
+
+  var yourSession = session.start(res, req);
+    
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  if(yourSession.testValue) {
+    res.write("Session Test Value: " + yourSession.testValue);
+  } else {
+    res.write("Hey, you should refresh your page.  If all is well, you will be display a session variable!");   
+  }
+  
+  res.end();
+  
+  yourSession.testValue = "It worked!";
+  
+  
+}).listen(process.env.C9_PORT, "0.0.0.0");
+console.log("Server running at http://" + host + ":" + port);
